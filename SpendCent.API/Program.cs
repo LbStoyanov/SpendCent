76517ed7dda844700b07ad0e8069f62767ae6a7a
LbStoyanov <!Ymm%PkM0g%clYso@5!B9>
2025-06-09 18:25:36 +0300

    Add project files.

---
+++ SpendCent.API/Program.cs
@@ -0,0 +1,28 @@
+using SpendCent.Core;
+using SpendCent.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+///Add services to the container.
+///
+
+builder.Services.AddInfrastructure();
+builder.Services.AddCore();
+
+//Add controllers 
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+//Routing configuration
+
+app.UseRouting();
+
+
+app.UseAuthentication();
+app.UseAuthorization();
+//Map controllers to endpoints
+app.MapControllers();
+
+app.Run();
